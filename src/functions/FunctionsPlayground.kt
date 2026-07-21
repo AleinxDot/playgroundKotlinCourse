bd53ead9b2d801b3acc0c8f43dce8e87f06fb59f
AleinxDot <agonzalesp@unasam.edu.pe>
2026-07-20 19:46:06 -0500

    add fortuneTeller function to provide daily fortune cookie messages and update feedFish function to use dynamic fish food selection

--- src/functions/FunctionsPlayground.kt
+++ src/functions/FunctionsPlayground.kt
@@ -19,12 +19,24 @@ class FunctionsPlayground {
     }
     fun feedFish(){
         val day = randomDay()
-        val food = "pellets"
+        val food = fishFood(day)
         println("Today is $day and the fish eat $food")
     }
     fun randomDay() : String{
         val week= listOf("Monday","Tuesday","Wednesday","Thursday", "Friday","Saturday","Sunday")
         return week[Random().nextInt(7)]
+    }
+    fun fishFood(day: String) : String{
+        return when (day) {
+            "Monday" -> "flakes"
+            "Tuesday" -> "pellets"
+            "Wednesday" -> "redworms"
+            "Thursday" -> "granules"
+            "Friday" -> "mosquitoes"
+            "Saturday" -> "lettuce"
+            "Sunday" -> "plankton"
+            else -> "fasting"
+        }
     }
 
 }
---
+++ src/functions/fortuneTeller.kt
@@ -0,0 +1,13 @@
+package functions
+
+fun main(args: Array<String>) {
+    println(getFortuneCookie())
+}
+fun getFortuneCookie() : String{
+    val fortunes = listOf("You will have a great day!","Things will go well for you today.","Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.", "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!", "Treasure your friends because they are your greatest fortune.");
+    println("Enter your birthday: ")
+    val birthday= readLine()?.toIntOrNull()
+    return "Your fortune is: ${fortunes[birthday?.rem(fortunes.size) ?: 0]}"
+}
