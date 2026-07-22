34ef6e6b611edd26bdac4d51fa5ccfac50f21fc4
AleinxDot <agonzalesp@unasam.edu.pe>
2026-07-22 01:34:51 -0500

    add ActivitySuggester and Functions modules for activity suggestions and fish feeding logic

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,16 @@
+import classes.Aquarium
+
 fun main() {
     //Configurar los parámetros desde la configuración de IntelliJ IDEA
     println("Hello Kotlin!")
+    buildAquarium()
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.height = 80
+    println("Length : ${myAquarium.length}\n " +
+            "Width : ${myAquarium.width}\n " +
+            "Height : ${myAquarium.height}")
+    println("Aquarium Volume : ${myAquarium.volume}%")
 }
